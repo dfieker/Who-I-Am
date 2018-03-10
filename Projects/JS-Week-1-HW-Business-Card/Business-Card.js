316b5977089eb4d5b1303d1a65e97afda4108047
dfieker <danielmfieker@gmail.com>
2018-03-10 08:58:06 -0600

    Added Business-Card.js

---
+++ Projects/JS-Week-1-HW-Business-Card/Business-Card.js
@@ -0,0 +1,14 @@
+
+var name = "         Daniel" 
+var company = "Company: OU"
+var twitter = "Twitter: None"
+var phone = "Phone:   405-405-4050"
+var address = "Address: 3322255910 Boomer Drive"
+var address2 = "         Norman, OK 77777"
+
+console.log(name)
+console.log(company)
+console.log(twitter)
+console.log(phone)
+console.log(address)
+console.log(address2)
